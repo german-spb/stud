e9635e54c6433f2c8dcbb1f5009df828414b0b6b
Герман <108299345+german-spb@users.noreply.github.com>
2024-07-09 16:51:00 +0300

    Update FileUploader.js

--- students-fe/src/components/FileUploader.js
+++ students-fe/src/components/FileUploader.js
@@ -1,86 +1,95 @@
 import React, { useState } from "react";
+import { API_URL } from "../constants";
 
-const MultipleFileUploader = () => {
-  const [files, setFiles] = useState(null) /*<FileList | null>(null)*/;
-  const [status, setStatus] = useState("initial") /*<
-    "initial" | "uploading" | "success" | "fail"
-  >("initial")*/;
-
-  const handleFileChange = (e) => {
+const SingleFileUploader =() => {
+  
+  const [status, setStatus] = useState("initial");
+  const [file, setFile] = useState();
+  
+  const  handleFileChange = (e) => {
     if (e.target.files) {
       setStatus("initial");
-      setFiles(e.target.files);
+      setFile(e.target.files[0]);
     }
   };
+  
 
-  const handleUpload = async () => {
-    if (files) {
+const  handleUpload = async () => {
+  
+    if (file) {
       setStatus("uploading");
-
+      
       const formData = new FormData();
-
-      [...files].forEach((file) => {
-        formData.append("files", file);
-      });
-
+      formData.append("name", (file.name).slice(0, file.name.lastIndexOf('.')));
+      formData.append("email", "german@mail.ru");
+      formData.append("document", file.type);
+      formData.append("phone", file.size);
+      
       try {
-        const result = await fetch("https://httpbin.org/post", {
+        const result = await fetch(API_URL, {
           method: "POST",
           body: formData,
+               
         });
-
-        const data = await result.json();
-
-        console.log(data);
-        setStatus("success");
+        
+        const data = await result.status;
+        console.log(data)
+        if (data === 201) {
+          setStatus("success");
+       window.location.reload();
+        }
+            
       } catch (error) {
         console.error(error);
         setStatus("fail");
       }
     }
+    
   };
+/*-------------------*/
 
   return (
     <>
       <div className="input-group">
         <label htmlFor="file" className="sr-only">
-          Choose files
+          Choose a file
         </label>
-        <input id="file" type="file" multiple onChange={handleFileChange} />
+        <input id="file" type="file" onChange={handleFileChange} />
       </div>
-      {files &&
-        [...files].map((file, index) => (
-          <section key={file.name}>
-            File number {index + 1} details:
-            <ul>
-              <li>Name: {file.name}</li>
-              <li>Type: {file.type}</li>
-              <li>Size: {file.size} bytes</li>
-            </ul>
-          </section>
-        ))}
-
-      {files && (
-        <button onClick={handleUpload} className="submit">
-          Upload {files.length > 1 ? "files" : "a file"}
-        </button>
+      {file && (
+        <section>
+          File details:
+          <ul>
+            <li>Name: {(file.name).slice(0, file.name.lastIndexOf('.'))}</li>
+            <li>Type: {file.type}</li>
+            <li>Size: {file.size} bytes</li>
+          </ul>
+        </section>
       )}
 
+      {file && (
+        <button onClick={handleUpload} className="submit">
+          Загрузить файл
+        </button>
+      )}
+      
+
       <Result status={status} />
+     
     </>
   );
 };
 
 const Result = ({ status }) => {
   if (status === "success") {
-    return <p>✅ Uploaded successfully!</p>;
+    return <p>✅ File uploaded successfully!</p>;
   } else if (status === "fail") {
-    return <p>❌ Upload failed!</p>;
+    return <p>❌ File upload failed!</p>;
   } else if (status === "uploading") {
-    return <p>⏳ Uploading started...</p>;
+    return <p>⏳ Uploading selected file...</p>;
   } else {
     return null;
   }
 };
 
-export default MultipleFileUploader;
+export default SingleFileUploader;
